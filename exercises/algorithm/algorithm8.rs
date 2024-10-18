2cd31575b30d5fc8813626de79cd933a8771b519
flora <flora_chen2021@163.com>
2024-10-18 16:13:55 +0800

    finish:traits4,5 algo5,6,7,8

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -1,15 +1,14 @@
 /*
-	queue
-	This question requires you to use queues to implement the functionality of the stac
+    queue
+    This question requires you to use queues to implement the functionality of the stac
 */
-// I AM NOT DONE
 
 #[derive(Debug)]
-pub struct Queue<T> {
+pub struct Queue<T: Clone> {
     elements: Vec<T>,
 }
 
-impl<T> Queue<T> {
+impl<T: Clone> Queue<T> {
     pub fn new() -> Queue<T> {
         Queue {
             elements: Vec::new(),
@@ -44,7 +43,7 @@ impl<T> Queue<T> {
     }
 }
 
-impl<T> Default for Queue<T> {
+impl<T: Clone> Default for Queue<T> {
     fn default() -> Queue<T> {
         Queue {
             elements: Vec::new(),
@@ -52,41 +51,53 @@ impl<T> Default for Queue<T> {
     }
 }
 
-pub struct myStack<T>
-{
-	//TODO
-	q1:Queue<T>,
-	q2:Queue<T>
+pub struct myStack<T:Clone> {
+    size: usize,
+    q1: Queue<T>,
+    q2: Queue<T>,
 }
-impl<T> myStack<T> {
+impl<T: Clone> myStack<T> {
     pub fn new() -> Self {
         Self {
-			//TODO
-			q1:Queue::<T>::new(),
-			q2:Queue::<T>::new()
+            size: 0,
+            q1: Queue::<T>::new(),
+            q2: Queue::<T>::new(),
         }
     }
     pub fn push(&mut self, elem: T) {
-        //TODO
+        self.q1.enqueue(elem.clone());
+        self.size += 1;
     }
     pub fn pop(&mut self) -> Result<T, &str> {
-        //TODO
-		Err("Stack is empty")
+        if self.size == 0 {
+            return Err("Stack is empty");
+        }
+        while self.q1.size() > 1 {
+            let elem = self.q1.dequeue().unwrap();
+            self.q2.enqueue(elem);
+        }
+        let result = self.q1.dequeue().unwrap();
+        self.size -= 1;
+        std::mem::swap(&mut self.q1, &mut self.q2);
+        Ok(result)
     }
     pub fn is_empty(&self) -> bool {
-		//TODO
-        true
+        if self.size == 0 {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
-	
-	#[test]
-	fn test_queue(){
-		let mut s = myStack::<i32>::new();
-		assert_eq!(s.pop(), Err("Stack is empty"));
+    use super::*;
+
+    #[test]
+    fn test_queue() {
+        let mut s = myStack::<i32>::new();
+        assert_eq!(s.pop(), Err("Stack is empty"));
         s.push(1);
         s.push(2);
         s.push(3);
@@ -100,5 +111,5 @@ mod tests {
         assert_eq!(s.pop(), Ok(1));
         assert_eq!(s.pop(), Err("Stack is empty"));
         assert_eq!(s.is_empty(), true);
-	}
+    }
 }
--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -13,7 +13,6 @@ trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-// TODO: Implement trait `AppendBar` for a vector of strings.
 
 impl AppendBar for Vec<String> {
     fn append_bar(self) -> Self {
